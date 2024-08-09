7b123381b7b68f93cd801701e17d277b3b85553e
Rajdeep Singh <radewala.2@gmail.com>
2024-08-09 18:00:47 +0530

    Card component

---
+++ components/Card/Card.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+import dayjs from 'dayjs';
+
+export default function Card({ title, author, date, url, category, image }: { title: string; author?: string; date?: Date | string | undefined; url: string, category?: string[] | undefined, image?: string | undefined }) {
+
+  return (
+    <div className="bg-fd-card shadow-sm rounded-lg fd-border overflow-hidden">
+      <div className="relative h-80">
+        {
+          image && <Image
+            className="h-80 w-full object-cover"
+            src={image}
+            alt={title}
+            height={324}
+            width={324}
+          />
+
+        }
+        {
+          category && <span className="absolute shadow bottom-5 right-5 text-sm font-semibold px-2.5 py-2 rounded bg-primary text-primary-foreground hover:bg-primary/90">
+            {category[0]}
+          </span>
+        }
+      </div>
+
+      <div className="flex flex-col items-start py-4 px-4">
+        <Link href={url} className="mb-4 text-xl font-bold md:text-2xl"> {title} </Link>
+        <div className="flex flex-row items-start text-sm text-gray-500 lg:items-center">
+          <p> Published by </p>
+          {author && <p className="font-bold mx-1.5"> {author} </p>}
+          {date && <p className="text-sm text-muted-foreground"> on {dayjs(date).format("MMMM DD, YYYY")} </p>}
+        </div>
+      </div>
+    </div>
+  )
+}
