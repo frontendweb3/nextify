4b2780aa826734e3e548e941566e041776e6f27c
Rajdeep Singh <radewala.2@gmail.com>
2024-08-09 21:36:36 +0530

    tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,7 @@ export default {
     './mdx-components.{ts,tsx}',
     './node_modules/fumadocs-ui/dist/**/*.js',
   ],
-  presets: [createPreset()],
+  presets: [createPreset({
+    addGlobalColors: true,
+  })],
 };
