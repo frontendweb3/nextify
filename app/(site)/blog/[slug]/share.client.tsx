fffd81ffaa48bd58165388122e163f91bc9246bb
Rajdeep Singh <radewala.2@gmail.com>
2024-08-11 22:07:38 +0530

    format the file

--- app/(site)/blog/[slug]/share.client.tsx
+++ app/(site)/blog/[slug]/share.client.tsx
@@ -1,13 +1,10 @@
 'use client';
-import {
-  Provider,
-  TooltipContent,
-  Tooltip,
-  TooltipTrigger,
-} from '@radix-ui/react-tooltip';
+
+import { Provider, TooltipContent, Tooltip, TooltipTrigger } from '@radix-ui/react-tooltip';
 import { useState } from 'react';
 
 export function Share({ url }: { url: string }): React.ReactElement {
+  
   const [open, setOpen] = useState(false);
 
   const onClick = (): void => {
@@ -19,11 +16,8 @@ export function Share({ url }: { url: string }): React.ReactElement {
     <div>
       <Provider disableHoverableContent={true}>
         <Tooltip open={open} onOpenChange={setOpen}>
-          <TooltipTrigger
-            className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10"
-            onClick={onClick}
-
-          >
+          <TooltipTrigger className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10"
+            onClick={onClick}>
             <>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -34,7 +28,7 @@ export function Share({ url }: { url: string }): React.ReactElement {
                 stroke="currentColor"
                 strokeWidth="2"
                 strokeLinecap="round"
-                stroke-linejoin="round"
+                strokeLinejoin="round"
                 className="h-5 w-5"
               >
                 <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path>
@@ -50,27 +44,5 @@ export function Share({ url }: { url: string }): React.ReactElement {
         </Tooltip>
       </Provider>
     </div>
-
   );
 }
-
-/* 
- *    <Tooltip open={open} onOpenChange={setOpen}>
-      <TooltipTrigger
-        className={cn(
-          buttonVariants({ className: 'gap-2', variant: 'secondary' }),
-        )}
-        onClick={onClick}
-      >
-        <Share className="size-4" />
-        Share Post
-      </TooltipTrigger>
-      <TooltipContent className="rounded-lg border bg-fd-popover p-2 text-sm text-fd-popover-foreground">
-        Copied
-      </TooltipContent>
-    </Tooltip>
-
- *
- *
- *
- * */
