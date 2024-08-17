25a7935e418d4e1729a14f62c1ffbd506da51162
Rajdeep Singh <radewala.2@gmail.com>
2024-08-17 19:46:41 +0530

    SEO

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,6 +2,18 @@ import { Hero } from '@/components/Hero/Hero';
 import { Work } from '@/components/work/work';
 import { FAQ } from '@/components/FAQ/FAQ';
 import { Testimonial } from '@/components/Testimonial/Testimonial';
+import { baseUrl, createMetadata } from '@/utils/metadata';
+
+
+export const metadata = createMetadata({
+  title: {
+    template: '%s | Nextify',
+    default: 'Home',
+  },
+  description: 'Nextify is a prebuilt template for building documentation sites using Next.js and Fumadocs.',
+  metadataBase: baseUrl,
+});
+
 
 export default function HomePage() {
   return (
