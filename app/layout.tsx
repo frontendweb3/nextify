5f6061032f8b7fa75be61c5b4f221fd68c8581c2
Rajdeep Singh <radewala.2@gmail.com>
2025-08-08 17:26:17 +0530

    provider

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import "@/app/global.css";
+import { Banner } from "fumadocs-ui/components/banner";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen">
+        <RootProvider
+          search={{
+            enabled: true,
+            options: {
+              links: [
+                ["Home", "/"],
+                ["About", "/about"],
+                ["Contact", "/contact"],
+              ],
+            },
+          }}
+        >
+          <Banner id="banner-1">
+            I failed my way to success - Thomas Edison
+          </Banner>
+          {children}
+        </RootProvider>
+      </body>
+    </html>
+  );
+}
